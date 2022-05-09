4b63eec6caf510b2b946284e892c5e8933f3c7fc
Hackerboy321 <100424168+Hackerboy321@users.noreply.github.com>
2022-05-09 18:53:19 +0530

    Create To find simple interst.py

---
+++ To find simple interst.py
@@ -0,0 +1,7 @@
+# All-Simple-Mathematics-Problem
+P= eval(input("Enter the principal balance :- "))
+R = eval(input("Enter the annual interst rate :- "))
+T = eval(input("Enter the time duration in year :- "))
+D= R/100
+Cal= P*(1+D*T)
+print("Final amount is"    u'\u20B9',Cal)
